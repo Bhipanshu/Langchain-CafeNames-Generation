94476c81c61e17ecccf42c24c9c6d78e011a0671
Bhipanshu <91373531+Bhipanshu@users.noreply.github.com>
2023-07-16 18:07:31 +0530

    Add files via upload

--- App-streamlit-run/app.py
+++ App-streamlit-run/app.py
@@ -1,1 +1,19 @@
+import streamlit as st
+import langchain_helper
 
+
+st.title("Cafe Name Generator")
+
+
+cuisine = st.sidebar.selectbox("Pick a Cuisine",("Indian","Italian","Mexican","Arabic","Chinese","European"))
+
+    
+    
+if st.sidebar.button("Generate"):
+    if cuisine != "":
+        response = langchain_helper.generate_restaurant_name_and_items(cuisine)
+        st.header(response['restaurant_name'].strip())
+        menu_items = response['menu_items'].strip().split(",")
+        st.write("**Menu Itmes**")
+        for item in menu_items:
+            st.write("-",item)
